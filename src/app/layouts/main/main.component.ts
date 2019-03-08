568c9329fc4b6f3b3388452e2d8074e4ba0b3461
jashion666 <shan41521>
2019-03-08 18:21:43 +0800

    更改导航样式，添加首页loading

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html'
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  loadingComplete = false;
+  percentage: number;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.loading();
+  }
+
+  logoutListener(event) {
+    if (event) {
+      this.loadingComplete = false;
+      this.loading();
+    }
+  }
+
+  loading() {
+    this.percentage = 0;
+    const loadingInterval = setInterval(() => {
+      if (this.percentage === 100) {
+        this.loadingComplete = true;
+        clearInterval(loadingInterval);
+      }
+      this.percentage = this.percentage + 50;
+    }, 1000);
   }
 
 }
--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {DEFAULT_LOGO} from '../../app.constants';
 
 @Component({
@@ -6,16 +6,21 @@ import {DEFAULT_LOGO} from '../../app.constants';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, AfterViewInit {
+export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
 
   visible = false;
   logo: string;
+  loadingInterval: any;
+  @Output() logoutEmitter = new EventEmitter();
 
   constructor() {
   }
 
   ngOnInit() {
     this.logo = DEFAULT_LOGO;
+    this.loadingInterval = setInterval(() => {
+      this.toggleLogo();
+    }, 10000);
   }
 
   open(): void {
@@ -26,10 +31,20 @@ export class NavbarComponent implements OnInit, AfterViewInit {
     this.visible = false;
   }
 
+  logout(): void {
+    this.logoutEmitter.emit(true);
+  }
+
   toggleLogo() {
     this.logo = this.logo === 'logo-toggle' ? 'logo' : 'logo-toggle';
   }
 
   ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if (this.loadingInterval) {
+      clearInterval(this.loadingInterval);
+    }
   }
 }
